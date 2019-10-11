767ab2aef19d832d6943edfe7834a5a09c528dcb
konaoki <kon.aoki.us@gmail.com>
2019-10-11 17:55:07 -0600

    added shells for classes

---
+++ src/Face.java
@@ -0,0 +1,8 @@
+public class Face{
+  public Face(){
+
+  }
+  public Vector getNormal(){
+    return null;
+  }
+}
---
+++ src/Renderer.java
@@ -0,0 +1,11 @@
+public class Renderer{
+  Vector[] DirectLightSources;
+  Vector[] indirectLightSources;
+  
+  public Renderer(Vector[] dls){
+    DirectLightSources=dls;
+  }
+  public void render(){
+
+  }
+}

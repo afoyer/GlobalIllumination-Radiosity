68b65f0aa6e925c37299c09a7e39bb9f9ac4820e
afoyer <34040599+afoyer@users.noreply.github.com>
2019-10-15 11:04:32 -0600

    Updated canvas

--- src/Canvas.java
+++ src/Canvas.java
@@ -42,39 +42,43 @@ public class Canvas extends JPanel
 		face3.setColor(Color.green);
 
 		Face areaLight = new Face(new Vector[]{new Vector(50,-300,50),new Vector(50,-300,0),new Vector(-50,-300,0),new Vector(-50,-300,50)});
-		areaLight.setColor(Color.white);
+		areaLight.setColor(new Color(250,230,175));
 		areaLight.generateDots(dotarea,1000);
 
 		face3.generateDots(dotarea,0);
-		Face face4 = new Face(new Vector[]{new Vector(100,-100,-200),new Vector(100,100,-200),new Vector(-0,100,-150),new Vector(-0,-100,-150)});
-		face4.setColor(Color.gray);
+		//CUBE
+		Face face4 = new Face(new Vector[]{new Vector(100,90,-200),new Vector(100,290,-200),new Vector(-0,290,-150),new Vector(-0,90,-150)});
+		face4.setColor(Color.white);
 		face4.generateDots(dotarea,0);
-		Face face5 = new Face(new Vector[]{new Vector(-100,100,-200),new Vector(-100,-100,-200),new Vector(0,-100,-150),new Vector(0,100,-150)});
-		face5.setColor(Color.gray);
+		Face face5 = new Face(new Vector[]{new Vector(-100,290,-200),new Vector(-100,90,-200),new Vector(0,90,-150),new Vector(0,290,-150)});
+		face5.setColor(Color.white);
 		face5.generateDots(dotarea,0);
-
 		int frameScale=100; //change this for camera frame size (smaller=more pixallated but less space in between pixels)
-		Face face6 = new Face(new Vector[]{new Vector(-0,100,-250),new Vector(0,-100,-250),new Vector(-100,-100,-200),new Vector(-100,100,-200)});
+		Face face6 = new Face(new Vector[]{new Vector(-0,290,-250),new Vector(0,90,-250),new Vector(-100,90,-200),new Vector(-100,290,-200)});
 		face6.setColor(Color.white);
 		face6.generateDots(dotarea,0);
-		Face face7 = new Face(new Vector[]{new Vector(100,100,-200),new Vector(100,-100,-200),new Vector(0,-100,-250),new Vector(0,100,-250)});
+		Face face7 = new Face(new Vector[]{new Vector(100,290,-200),new Vector(100,90,-200),new Vector(0,90,-250),new Vector(0,290,-250)});
 		face7.setColor(Color.white);
 		face7.generateDots(dotarea,0);
-		Face face8 = new Face(new Vector[]{new Vector(100,-100,-200),new Vector(0,-100,-150),new Vector(-100,-100,-200), new Vector(0,-100,-250)});
+		Face face8 = new Face(new Vector[]{new Vector(100,90,-200),new Vector(0,90,-150),new Vector(-100,90,-200), new Vector(0,90,-250)});
 		face8.setColor(Color.white);
 		face8.generateDots(dotarea,0);
-		Face face9 = new Face(new Vector[]{new Vector(0,100,-250),new Vector(-100,100,-200),new Vector(0,100,-150),new Vector(100,100,-200)});
+		Face face9 = new Face(new Vector[]{new Vector(0,290,-250),new Vector(-100,290,-200),new Vector(0,290,-150),new Vector(100,290,-200)});
 		face9.setColor(Color.white);
 		face9.generateDots(dotarea,0);
+		//FLOOR AND ROOF
 		Face face10 = new Face(new Vector[]{new Vector(-300,300,-300),new Vector(300,300,-300),new Vector(300,300,0),new Vector(-300,300,0)});
 		face10.setColor(Color.white);
 		face10.generateDots(dotarea,0);
+		Face face11 = new Face(new Vector[]{new Vector(-300,-300,0),new Vector(300,-300,0),new Vector(300,-300,-300),new Vector(-300,-300,-300)});
+		face11.setColor(Color.white);
+		face11.generateDots(dotarea,0);
 		int frameWidth=10*frameScale;
 		int frameHeight=10*frameScale;
 		int frameDepth=3*frameScale;
 		cameraFrame = new CameraFrame(new Vector[]{new Vector(frameWidth/2,-frameHeight/2,-frameDepth),new Vector(frameWidth/2,frameHeight/2,-frameDepth),new Vector(-frameWidth/2,frameHeight/2,-frameDepth),new Vector(-frameWidth/2,-frameHeight/2,-frameDepth)});
 
-		Face[] faces = new Face[]{face,face2, face3, face4, face5, face6, face7, face8, face9, face10, areaLight};
+		Face[] faces = new Face[]{face,face2, face3, face4, face5, face6, face7, face8, face9, face10, face11, areaLight};
 		camera = new Camera(Vector.origin,cameraFrame,faces);
 		renderer = new Renderer();
 		renderer.loadFaces(faces);
